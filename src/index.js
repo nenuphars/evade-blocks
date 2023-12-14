e29fd2486c99b623cfe77848a465a738c640436e
Nadine Rangosch <nrangosch@gmail.com>
2023-12-14 20:49:00 +0100

    player moving with keyboard input

--- src/index.js
+++ src/index.js
@@ -1,47 +1,57 @@
 class Game {
-    constructor(){
-        this.player = null
-        
-    }
-    start(){
-        this.player = new Player()
-    }
+  constructor() {
+    this.player = null;
+  }
+  start() {
+    this.player = new Player();
+    this.attachEventListeners()
+  }
+  attachEventListeners(){
+    window.addEventListener("keydown", (event)=> {
+        const input = event.key
+        if(input === "ArrowLeft"){
+            this.player.moveLeft()
+        }
+        else if(input === "ArrowRight"){
+            this.player.moveRight()
+        }
+    })
+  }
 }
 
 class Player {
-    constructor(){
-        this.width = 10
-        this.height = 5
-        this.positionX = 50 -this.width / 2
-        this.positionY = 0
-        
-        this.domElement = this.createPlayer()
-    }
+  constructor() {
+    this.width = 10;
+    this.height = 5;
+    this.positionX = 50 - this.width / 2;
+    this.positionY = 0;
 
-    createPlayer(){
-        const nodeDOM = document.createElement("div")
-        nodeDOM.id = "player"
-        nodeDOM.style.width = `${this.width}vh`
-        nodeDOM.style.height = `${this.height}vw`
-        nodeDOM.style.bottom = this.positionY + `vh`
-        nodeDOM.style.left = `${this.positionX}vw`
+    this.domElement = this.createPlayer();
+  }
 
-        const board = document.getElementById("board")
-        board.appendChild(nodeDOM)
-        
-        return nodeDOM
-    }
+  createPlayer() {
+    const nodeDOM = document.createElement("div");
+    nodeDOM.id = "player";
+    nodeDOM.style.width = `${this.width}vh`;
+    nodeDOM.style.height = `${this.height}vw`;
+    nodeDOM.style.bottom = this.positionY + `vh`;
+    nodeDOM.style.left = `${this.positionX}vw`;
 
-    moveRight(){
-        this.positionX += 1
-        this.domElement.style.left = this.positionX + 'vw'
-    }
+    const board = document.getElementById("board");
+    board.appendChild(nodeDOM);
+    return nodeDOM;
+  }
 
-    moveLeft(){
-        this.positionX -= 1
-        this.domElement.style.left = this.positionX + 'vw'
-    }
+  moveRight() {
+    this.positionX += 1;
+    this.domElement.style.left = `${this.positionX}vw`;
+  }
+
+  moveLeft() {
+    this.positionX -= 1;
+    this.domElement.style.left = `${this.positionX}vw`;
+  }
 }
 
-const game = new Game()
-game.start()
+const game = new Game();
+game.start();
